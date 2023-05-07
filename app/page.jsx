c65b047a5fd9141d4c6b434db56c232317c76770
Akash <akashkumarsingh11032001@gmail.com>
2023-05-07 20:28:19 +0530

    ./

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from '@components/Feed';
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -10,7 +12,7 @@ const Home = () => {
             Prompts Generator is an open-source AI prompting tool for moder world to discover, create and share creative prompts.
         </p>
 
-        {/* Feed */}
+        <Feed />
 
     </section>
   )
